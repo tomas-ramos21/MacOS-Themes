231c7f642b388e88b1719919d3358ad1c2fa2fa2
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-02-28 11:42:55 +0800

    Added Doom theme

--- themer.sh
+++ themer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "${0%/*}"
 
-THEMES=("FreeBSD" "Nord" "Blossom", "Dracula", "Monokai")
+THEMES=("FreeBSD" "Nord" "Blossom", "Dracula", "Monokai", "Doom")
 UBERSICHT="/Users/tomasramos/.config/ubersicht/spaces.coffee"
 ALACRITTY="/Users/tomasramos/.config/alacritty/alacritty.yml"
 YABAI="/Users/tomasramos/.config/yabai/yabairc"
