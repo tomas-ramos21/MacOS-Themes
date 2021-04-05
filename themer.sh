ff0ae5dd48992d23d00c7cc85c7898fcfed0c8f9
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-04-05 22:40:26 +0800

    Added light theme

--- themer.sh
+++ themer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "${0%/*}"
 
-THEMES=("FreeBSD" "Nord" "Blossom", "Dracula", "Monokai", "Doom")
+THEMES=("FreeBSD" "Nord" "Blossom", "Dracula", "Monokai", "Doom", "Light")
 UBERSICHT="/Users/tomasramos/.config/ubersicht/spaces.coffee"
 ALACRITTY="/Users/tomasramos/.config/alacritty/alacritty.yml"
 YABAI="/Users/tomasramos/.config/yabai/yabairc"
